e3e042e70cfa438e634df6c7aca0f3da192d27b9
AngeZanetti <ange.zanetti@gmail.com>
2011-08-05 12:44:33 +0200

    TweetBox v0.2
    Now the tweetbox get the Twitter status in a JSON file

--- TweetBox.js
+++ TweetBox.js
@@ -1,84 +1,81 @@
-// // ///////////////////////////////////////////////////////////////////////////////////
-//  TweetBox, a nice Arduino box which can tweet throw a Node.js Server !
-// Copyright (C) 2012  CoworkingLille - @AngeZanetti & @Guitool
-// 
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-// 
-// ///////////////////////////////////////////////////////////////////////////////////
+// // ///////////////////////////////////////////////////////////////////////////////// //
+//  TweetBox, a nice Arduino box which can tweet throw a Node.js Server !				//
+// Copyright (C) 2012  AngeZanetti - made with & @Guitool for CoworKingLille			//	
+// 																						//
+// This program is free software; you can redistribute it and/or						//	
+// modify it under the terms of the GNU General Public License							//
+// as published by the Free Software Foundation; either version 2						//
+// of the License, or (at your option) any later version.								//
+// 																						//
+// This program is distributed in the hope that it will be useful,						//
+// but WITHOUT ANY WARRANTY; without even the implied warranty of						//
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the						//
+// GNU General Public License for more details.											//
+// 																						//
+// You should have received a copy of the GNU General Public License					//
+// along with this program; if not, write to the Free Software							//
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.		//	
+// //////////////////////////////////////////////////////////////////////////////////// //
 
+// Tweetbox requires express & Tweasy to works. Install it with NPM
 var express = require('express');
 var sys = require('sys')
   , tweasy = require("tweasy")
   , OAuth = require("oauth").OAuth
   ;
-// Les données necessaires pour l'Oauth Twitter
-var my_consumer_key=" ";
-var my_consumer_secret= " "; 
+var fs = require('fs');
+var sys = require('sys');
 
+// The Oauth values, you can found it on dev.twitter.com
+var my_consumer_key="0";
+var my_consumer_secret= "0"; 
 var oauthConsumer = new OAuth(
     "http://twitter.com/oauth/request_token",
     "http://twitter.com/oauth/access_token", 
     my_consumer_key,  my_consumer_secret, 
     "1.0", null, "HMAC-SHA1");
 var twitterClient = tweasy.init(oauthConsumer, {
-  access_token : " ",//my_users_access_token,
-  access_token_secret : " ",//my_users_access_token_secret
+  access_token : "0",
+  access_token_secret : "0",
 });
-// Lancement du serveur Node.js
+// Create the Node.js Server with express
 var app = express.createServer(
       express.logger()
     , express.bodyParser()
  );
 
-var tweetJSON = {"ArduinoVar": [
-        "L'espace de #CoworkingLille est ouvert ! ",
-		"Le café du #CoworkingLille est pret ! ",
-		"Période de concentration profonde au #CoworkingLille",
-		"Musique et ambiance festive ! #coworkinglille", 
-		"C'est l'heure de l'apéro... Envie d'un verre ? #coworkinglille", 
-		"On déjeune ? Qui vient Coluncher ce midi ! #coworkinglille",
-		"Ouverture de la session Atelier. #coworkinglille",
-		"On a bien bossé, on ferme ! On se voit demain ? #coworkinglille"
-	    ]
-};
-
-//On repuère les params 
-app.get('/:toto', function(req, res){ // rajouter /:tata si besoin d'une deuxième var	
-    //console.log(req);
+// When the client call the page:3000/var1
+app.get('/:toto', function(req, res){ // we get the var, named it toto :)
     var toto = req.params["toto"];
-    console.log(toto);
-	// On récupère le statut correspondant à la valeur du param
-	//var status = JSON.parse(tweetJSON); Parser que si on importe le fichier
-	var status = tweetJSON.ArduinoVar[toto];
-	// Fonction qui permet de twitter
+	var status;
+	// We get the date also to make our tweet unique - or sort of. To shunt the API limitations
 	var time = new Date();
+	
+	// Use Tweasy to tweet easily. 
 	time = time.getHours() + ":" + time.getMinutes();
-    twitterClient.updateStatus("[TestEnCours] " + "Il est " + time + ", " + status, 
-	  function(er, resp){
-	    // Si ça passe on confirme l'nvoi du tweet
-		if (!er) {
-	      sys.puts("you tweeted!")
-	    }
-		//Sinon message err
-		else {
-			console.log(er);
-			}
-	  });
-	// Message affiché HTML
-	res.send('hello world');
+	// We open & read the JSON file to get the status connected with the var
+	fs.readFile('valeurPotentio.txt', function (err, data) {
+	  	if (err) throw err;
+	  	list = JSON.parse(data);
+		status = list.cwlStatus[toto];
+		
+		twitterClient.updateStatus("[TestEnCours] " + "Il est " + time + ", " + status, 
+		  function(er, resp){
+		    // If it worked you get a nice "you tweet in your console"
+			if (!er) {
+		      sys.puts("you tweeted!")
+		    }
+			//Else you got a error message
+			else {
+				console.log(er);
+				}
+		  });
+	});
+	
+    
+	// The text diaplay on the webpage - totally unusefull !
+	res.send('TweetBox rules ! ');
   
 });
-// Le serv écoute sur le port 3000
+// Our server wait the connecitons on the 3000 port.
 app.listen(3000);
